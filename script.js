4179cad6f4cdbc575371f34451b4b4a7ff5c4ca5
Weat-ctrl <aaaaaaarrrrrrrgghhh@gmail.com>
2025-04-02 19:42:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.155.0/build/three.module.js';
+import \* as THREE from 'https://unpkg.com/three@0.155.0/build/three.module.js';
 import { GLTFLoader } from 'https://unpkg.com/three@0.155.0/examples/jsm/loaders/GLTFLoader.js';
 
 // Scene setup
